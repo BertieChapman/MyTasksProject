018b4c0720df1ab1c416dbfe545ff7a28f723842
Bertrand Chapman <bertrand_chapman@hotmail.com>
2018-05-19 09:10:44 +1000

    Init commit

---
+++ app/src/main/java/com/bertrandechapman/mytasksproject/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.bertrandechapman.mytasksproject;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+
+    }
+}
+
